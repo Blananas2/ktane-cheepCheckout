d4b454fc72f689c834e9513897942c4afb2a1822
thunder725 <lloustalot@artfx.fr>
2026-08-20 21:22:23 +0200

    Added Ruleseed Support

--- Assets/cheepCheckoutScript.cs
+++ Assets/cheepCheckoutScript.cs
@@ -11,6 +11,7 @@ public class cheepCheckoutScript : MonoBehaviour
 
     public KMBombInfo Bomb;
     public KMAudio Audio;
+    public KMRuleSeedable Ruleseed;
 
     public KMSelectable[] MainButtons;
     public KMSelectable[] OtherButtons; //disp, left, right, clear, submit
@@ -19,12 +20,96 @@ public class cheepCheckoutScript : MonoBehaviour
     public Color[] Colors; //white, red, yellow, green
 
     //KMAudio.KMAudioRef soundEffect;
-    private List<string> birdNames = new List<string> { "Auklet", "Bluebird", "Chickadee", "Dove", "Egret", "Finch", "Godwit", "Hummingbird", "Ibis", "Jay", "Kinglet", "Loon", "Magpie", "Nuthatch", "Oriole", "Pipit", "Quail", "Raven", "Shrike", "Thrush", "Umbrellabird", "Vireo", "Warbler", "Xantus’s Hummingbird", "Yellowlegs", "Zigzag Heron", "[Unicorn Bastard]" };
-    private List<int> birdPrices = new List<int> { 359, 633, 199, 250, 901, 690, 527, 912, 410, 893, 728, 123, 377, 99, 314, 141, 904, 800, 420, 260, 1, 967, 369, 551, 201, 753, 0 };
-    private List<int> birdPitches = new List<int> { 1, 1, 0, 2, 0, 1, 1, 1, 2, 1, 0, 2, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 2, 0, 0, 0, 2, 0, 0, 2, 1, 1, 2, 1, 2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 2, 2, 1, 2, 2, 2, 1, 1, 2, 1, 0, 2, 2, 1, 2, 0, 2, 1, 0, 0, 1, 2, 0, 0, 1, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0 };
-    private List<int> numberList = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
-    private List<string> mainNumbers = new List<string> { ".01", ".05", ".10", ".25", " 1 ", " 5 ", "10", "25" };
-    private List<string> pitchNames = new List<string> { "Low", "Medium", "High" };
+    /// <summary> All available bird names, sorted by first letter, with the first name in each being Ruleseed 1. </summary>
+    private readonly string[][] birdNames = new string[27][] {
+        new string[5] { "Auklet", "Albatross", "Antbird", "Apalis", "Aracari" },
+        new string[5] { "Bluebird", "Bird-of-Paradise", "Bittern", "Bananaquit", "Booby" },
+        new string[5] { "Chickadee", "Crane", "Cuckoo", "Courser", "Chachalaca" },
+        new string[5] { "Dove", "Darter", "Duck", "Drongo", "Doradito" },
+        new string[4] { "Egret", "Emu", "Eagle", "Eremomela" },
+        new string[5] { "Finch", "Flamingo", "Frigatebird", "Firefinch", "Friarbird" },
+        new string[5] { "Godwit", "Grouse", "Guan", "Gull", "Go-Away Bird" },
+        new string[4] { "Hummingbird", "Hoatzin", "Hornbill", "Heron" },
+        new string[4] { "Ibis", "Inezia", "Iora", "Indigobird" },
+        new string[4] { "Jay", "Jacamar", "Jacana", "Jacobin" },
+        new string[5] { "Kinglet", "Kiwi", "Kagu", "Kingfisher", "Kea" },
+        new string[5] { "Loon", "Lark", "Lyrebird", "Leaftosser", "Limpkin" },
+        new string[5] { "Magpie", "Mallard", "Motmot", "Murrelet", "Morepork" },
+        new string[4] { "Nuthatch", "Nightingale", "Noddy", "Nightjar" },
+        new string[4] { "Oriole", "Ostrich", "Oilbird", "Owl" },
+        new string[5] { "Pipit", "Pardalote", "Pheasant", "Potoo", "Pigeon" },
+        new string[2] { "Quail", "Quetzal" },
+        new string[5] { "Raven", "Rhea", "Robin", "Roadrunner", "Riflebird" },
+        new string[5] { "Shrike", "Scrubbird", "Seedsnipe", "Seriema", "Shearwater" },
+        new string[5] { "Thrush", "Turkey", "Turaco", "Tern", "Tūī" },
+        new string[2] { "Umbrellabird", "Ural Owl" },
+        new string[3] { "Vireo", "Vulture", "Vanga" },
+        new string[5] { "Warbler", "Wren", "Woodpecker", "Wagtail", "Weaver" },
+        new string[2] { "Xantus’s Hummingbird", "Xavier's Greenbul" },
+        new string[3] { "Yellowlegs", "Yellowthroat", "Yuhina" },
+        new string[3] { "Zigzag Heron", "Zebra Dove", "Zino's Petrel" },
+        new string[1] { "[Unicorn Bastard]" }
+    };
+
+    /// <summary> All base bird prices, in order for Ruleseed 1. </summary>
+    private readonly List<int> birdPrices = new List<int> { 359, 633, 199, 250, 901, 690, 527, 912, 410, 893, 728, 123, 377, 99, 314, 141, 904, 800, 420, 260, 1, 967, 369, 551, 201, 753};
+
+    /// <summary> All 27 possible bird pitches in the order of the Ruleseed 1 manual. </summary>
+    private readonly int[][] birdPitches = new int[27][] {
+        new int[3] { 1, 1, 0 }, 
+        new int[3] { 2, 0, 1 }, 
+        new int[3] { 1, 1, 2 }, 
+        new int[3] { 1, 0, 2 }, 
+        new int[3] { 0, 0, 1 }, 
+        new int[3] { 1, 1, 1 }, 
+        new int[3] { 0, 1, 1 }, 
+        new int[3] { 0, 1, 0 }, 
+        new int[3] { 2, 0, 0 }, 
+        new int[3] { 0, 2, 0 }, 
+        new int[3] { 0, 2, 1 }, 
+        new int[3] { 1, 2, 1 }, 
+        new int[3] { 2, 1, 2 }, 
+        new int[3] { 0, 0, 2 }, 
+        new int[3] { 1, 0, 1 }, 
+        new int[3] { 0, 2, 2 }, 
+        new int[3] { 1, 2, 2 }, 
+        new int[3] { 2, 1, 1 }, 
+        new int[3] { 2, 1, 0 }, 
+        new int[3] { 2, 2, 1 }, 
+        new int[3] { 2, 0, 2 }, 
+        new int[3] { 1, 0, 0 }, 
+        new int[3] { 1, 2, 0 }, 
+        new int[3] { 0, 1, 2 }, 
+        new int[3] { 2, 2, 2 }, 
+        new int[3] { 2, 2, 0 }, 
+        new int[3] { 0, 0, 0 },
+    };
+
+    /// <summary> Now that Ruleseed is present, represents the name of the 27 possible birds, one for each letter in order. </summary>
+    private string[] ruleseededBirdNames = new string[27];
+
+    /// <summary> Now that Ruleseed is present, is used as an index to know the shuffled order of the Bird Pitches. </summary>
+    private List<int> ruleseededBirdPitchesOrder;
+
+    /// <summary> Now that Ruleseed is present, represents the new Bird Prices in order, with added variance. </summary>
+    private List<int> ruleseededBirdPrices = new List<int>();
+
+    /// <summary> Does Ruleseed ask for the presence of Cheap Checkout instead of a specific indicator? </summary>
+    private bool ruleseedUnicornIsCheapCheckout;
+    // <summary> Does Ruleseed ask for a Lit indicator? </summary>
+    private bool ruleseedUnicornIndicatorIsLit;
+    // <summary> Which Indicator does ruleseed check for? </summary>
+    private int ruleseedUnicornIndicatorType;
+
+    private readonly string[] possibleIndicators = new string[11]{"SND", "CLR", "CAR", "IND", "FRQ", "SIG", "NSA", "MSA", "TRN", "BOB", "FRK"};
+
+
+    /// <summary> Index of the 27 possible birds, with Unicorn being the 27th </summary>
+    private int[] numberArray = new int[27] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
+
+    private readonly List<string> mainNumbers = new List<string> { ".01", ".05", ".10", ".25", " 1 ", " 5 ", "10", "25" };
+    
+    private readonly List<string> pitchNames = new List<string> { "Low", "Medium", "High" };
     int selectedBird = 0;
     int pressedButton = 0;
     int unicornPressCount = 0;
@@ -37,6 +122,10 @@ public class cheepCheckoutScript : MonoBehaviour
     bool hasUnicornBird = false;
     bool unicorn = false;
     bool waiting = false;
+
+
+    // Data easily readable for Souvenir now that we have ruleseed
+    private string SouvenirBirdsPitches;
 
     //Logging
     static int moduleIdCounter = 1;
@@ -73,16 +162,21 @@ public class cheepCheckoutScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        numberList.Shuffle();
-        birdPrice = birdPrices[numberList[0]] + birdPrices[numberList[1]] + birdPrices[numberList[2]] + birdPrices[numberList[3]] + birdPrices[numberList[4]];
-        Debug.Log("<Cheep Checkout>" + birdNames[numberList[0]] + " + " + birdNames[numberList[1]] + " + " + birdNames[numberList[2]] + " + " + birdNames[numberList[3]] + " + " + birdNames[numberList[4]] + " = " + birdPrice);
+        numberArray.Shuffle();
+
+        ManageRuleseed();
+        ManageRuleseededSouvenir();
+
+        birdPrice = ruleseededBirdPrices[numberArray[0]] + ruleseededBirdPrices[numberArray[1]] + ruleseededBirdPrices[numberArray[2]] + ruleseededBirdPrices[numberArray[3]] + ruleseededBirdPrices[numberArray[4]];
+        Debug.Log("<Cheep Checkout>" + ruleseededBirdNames[numberArray[0]] + " + " + ruleseededBirdNames[numberArray[1]] + " + " + ruleseededBirdNames[numberArray[2]] + " + " + ruleseededBirdNames[numberArray[3]] + " + " + ruleseededBirdNames[numberArray[4]] + " = " + birdPrice);
 
         for (int k = 0; k < 5; k++)
         {
-            Debug.LogFormat("[Cheep Checkout #{0}] Bird {1}: {2}, {3}, {4} ({5}, costs ${6})", moduleId, k + 1, pitchNames[birdPitches[numberList[k] * 3]], pitchNames[birdPitches[numberList[k] * 3 + 1]], pitchNames[birdPitches[numberList[k] * 3 + 2]], birdNames[numberList[k]], dollar(birdPrices[numberList[k]]));
+            int[] _birdPitch = birdPitches[ruleseededBirdPitchesOrder[numberArray[k]]];
+            Debug.LogFormat("[Cheep Checkout #{0}] Bird {1}: {2}, {3}, {4} ({5}, costs ${6})", moduleId, k + 1, pitchNames[_birdPitch[0]], pitchNames[_birdPitch[1]], pitchNames[_birdPitch[2]], ruleseededBirdNames[numberArray[k]], dollar(ruleseededBirdPrices[numberArray[k]]));
         }
 
-        if (numberList[0] == 26 || numberList[1] == 26 || numberList[2] == 26 || numberList[3] == 26 || numberList[4] == 26)
+        if (numberArray[0] == 26 || numberArray[1] == 26 || numberArray[2] == 26 || numberArray[3] == 26 || numberArray[4] == 26)
         {
             hasUnicornBird = true;
             Debug.Log("<Cheep Checkout>" + "UNICORN BIRD DETECTED");
@@ -93,9 +187,27 @@ public class cheepCheckoutScript : MonoBehaviour
 
         answerPrice = customerPrice - birdPrice;
 
-        if (Bomb.IsIndicatorOn(Indicator.BOB) && hasUnicornBird)
+
+
+        // Check for Unicorn
+        if (ruleseedUnicornIsCheapCheckout)
         {
-            unicorn = true;
+            unicorn = Bomb.GetSolvableModuleNames().Contains("CheapCheckoutModule");
+        }
+        else
+        {
+            if (ruleseedUnicornIndicatorIsLit)
+            {
+                unicorn = Bomb.IsIndicatorOn((Indicator)ruleseedUnicornIndicatorType);
+            }
+            else
+            {
+                unicorn = Bomb.IsIndicatorOff((Indicator)ruleseedUnicornIndicatorType);
+            }
+        }
+
+        if (hasUnicornBird && unicorn)
+        {
             Debug.Log("<Cheep Checkout>" + "UNICORN IN EFFECT");
             Debug.LogFormat("[Cheep Checkout #{0}] Unicorn rule applies, please repeatedly slap the customer.", moduleId);
         }
@@ -113,6 +225,104 @@ public class cheepCheckoutScript : MonoBehaviour
                 Debug.LogFormat("[Cheep Checkout #{0}] Correct change is ${1}.", moduleId, dollar(answerPrice));
             }
         }
+    }
+
+    void ManageRuleseed()
+    {
+        MonoRandom Rng = Ruleseed.GetRNG();
+
+        // Those values are used as-is for ruleseed 1 or shuffled afterwards
+        ruleseededBirdPitchesOrder = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
+
+        // This specifically contains only 26 values, because we only shuffle the usable ones;
+        ruleseededBirdPrices = birdPrices;
+
+        // Ruleseed 1, initialize default values
+        if (Rng.Seed == 1)
+        {
+            for (int i = 0; i < 27; i ++)
+            {
+                ruleseededBirdNames[i] = birdNames[i][0];
+            }
+
+            // Add the Unicorn Bastard's values back
+            ruleseededBirdPrices.Insert(26, 0);
+
+            // Manage unicorn => Lit BOB present
+            ruleseedUnicornIsCheapCheckout = false;
+            ruleseedUnicornIndicatorIsLit = true;
+            ruleseedUnicornIndicatorType = 9;
+            return;
+        }
+
+
+        Debug.LogFormat("[Cheep Checkout #{0}] Using Ruleseed {1}:", moduleId, Rng.Seed);
+
+        // Following the order of the manual:
+        // First the Pitches using a single FisherYates
+        // Then the Prices using a single FisherYates
+        // Then 26 times:
+        //      One FisherYates for the letter
+        //      One rnd.next(-10, 11) for price variance
+        // Then one rnd.next(0, 10) for Unicorn condition
+        // If != 10
+        //      One rnd.next(0, 2) for lit/unlit choice
+        //      One rnd.next(0, 11) for Indicator selection
+
+        Rng.ShuffleFisherYates(ruleseededBirdPitchesOrder);
+
+        ruleseededBirdPrices = birdPrices;
+        Rng.ShuffleFisherYates(ruleseededBirdPrices);
+
+        string[] namesForThisLetter;
+        for (int i = 0; i < 26; i ++)
+        {
+            namesForThisLetter = birdNames[i];
+            Rng.ShuffleFisherYates(namesForThisLetter);
+            ruleseededBirdNames[i] = namesForThisLetter[0];
+
+            ruleseededBirdPrices[i] += Rng.Next(-10, 11);
+            if (ruleseededBirdPrices[i] < 1) ruleseededBirdPrices[i] = 1;
+        }
+
+        // Unicorn Bastard is not present in the manual, so it must be handled separately here
+        ruleseededBirdNames[26] = birdNames[26][0];
+        ruleseededBirdPrices.Insert(26, 0);
+
+        // Pitch is already managed as we want to randomize the unicorn's cry too, so no need to care here
+
+        Debug.Log("<Cheep Checkout>" + ruleseededBirdNames.Join(" // "));
+        Debug.Log("<Cheep Checkout>" + ruleseededBirdPrices.Join(" // "));
+        Debug.Log("<Cheep Checkout>" + ruleseededBirdPitchesOrder.Select(x => birdPitches[x].Join()).Join(" // "));
+
+        if (Rng.Next(0, 10) == 0)
+        {
+            ruleseedUnicornIsCheapCheckout = true;
+            Debug.Log("<Cheep Checkout>" + "UNICORN IS CHEAP CHECKOUT PRESENT");
+        }
+        else
+        {
+            ruleseedUnicornIsCheapCheckout = false;
+            ruleseedUnicornIndicatorIsLit = Rng.Next(0, 2) == 0;
+            ruleseedUnicornIndicatorType = Rng.Next(0, 11);
+
+            Debug.Log("<Cheep Checkout>" + "UNICORN IS " + (ruleseedUnicornIndicatorIsLit ? "LIT " : "UNLIT ") + possibleIndicators[ruleseedUnicornIndicatorType] + " PORT");
+        }
+    }
+
+    /// <summary> Souvenir uses a constant string for its data; so we're just gonna replace it in the format it needs with our new data </summary>
+    void ManageRuleseededSouvenir()
+    {
+        char[] souvenirPitchConversion = new char[3] { 'L', 'M', 'H'};
+
+        SouvenirBirdsPitches = "";
+
+        for (int i = 0; i < 27; i ++)
+        {
+            SouvenirBirdsPitches += ruleseededBirdNames[i] + '=' + birdPitches[ruleseededBirdPitchesOrder[i]].Select(x => souvenirPitchConversion[x]).Join("") + ';';
+        }
+
+        Debug.LogFormat("<Cheep Checkout #{0}> SOUVENIR PITCHES DATA STRING: {1}", moduleId, SouvenirBirdsPitches);
     }
 
     string dollar (int n) {
@@ -295,50 +505,23 @@ public class cheepCheckoutScript : MonoBehaviour
 
     IEnumerator PlaySounds()
     {
-        Debug.Log("<Cheep Checkout>" + birdPitches[numberList[selectedBird] * 3]);
-        if (birdPitches[numberList[selectedBird] * 3] == 0)
-        {
-            Audio.PlaySoundAtTransform("low1", transform);
-        }
-        else if (birdPitches[numberList[selectedBird] * 3] == 1)
-        {
-            Audio.PlaySoundAtTransform("med1", transform);
-        }
-        else if (birdPitches[numberList[selectedBird] * 3] == 2)
-        {
-            Audio.PlaySoundAtTransform("high1", transform);
-        }
+        int[] _birdPitches = birdPitches[ruleseededBirdPitchesOrder[numberArray[selectedBird]]];
 
-        yield return new WaitForSeconds(0.68f);
+        for (int i = 0; i < 3; i ++)
+        {
+            Debug.Log("<Cheep Checkout>" + _birdPitches[i]);
 
-        Debug.Log("<Cheep Checkout>" + birdPitches[numberList[selectedBird] * 3 + 1]);
-        if (birdPitches[numberList[selectedBird] * 3 + 1] == 0)
-        {
-            Audio.PlaySoundAtTransform("low2", transform);
-        }
-        else if (birdPitches[numberList[selectedBird] * 3 + 1] == 1)
-        {
-            Audio.PlaySoundAtTransform("med2", transform);
-        }
-        else if (birdPitches[numberList[selectedBird] * 3 + 1] == 2)
-        {
-            Audio.PlaySoundAtTransform("high2", transform);
-        }
+            switch (_birdPitches[i])
+            {
+                case 0: Audio.PlaySoundAtTransform("low1", transform); break;
+                case 1: Audio.PlaySoundAtTransform("med1", transform); break;
+                case 2: Audio.PlaySoundAtTransform("high1", transform); break;
+            }
 
-        yield return new WaitForSeconds(0.68f);
-
-        Debug.Log("<Cheep Checkout>" + birdPitches[numberList[selectedBird] * 3 + 2]);
-        if (birdPitches[numberList[selectedBird] * 3 + 2] == 0)
-        {
-            Audio.PlaySoundAtTransform("low3", transform);
-        }
-        else if (birdPitches[numberList[selectedBird] * 3 + 2] == 1)
-        {
-            Audio.PlaySoundAtTransform("med3", transform);
-        }
-        else if (birdPitches[numberList[selectedBird] * 3 + 2] == 2)
-        {
-            Audio.PlaySoundAtTransform("high3", transform);
+            if (i != 2)
+            { yield return new WaitForSeconds(0.68f); }
+            else
+            { yield return null; }
         }
     }
 
